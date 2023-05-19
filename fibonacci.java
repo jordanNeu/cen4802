afe9f962e8905d407ce2cfcd95dfcb583bfd1e86
Jordan Neumann <Jneumann26@gmail.com>
2023-05-19 10:09:24 -0400

    Added Fibonacci Program with Recursion

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+public class fibonacci {
+    static long recursion (long n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return recursion(n - 1) + recursion (n - 2);
+        }
+    }
+    public static void main (String[] args) {
+//        iterate(15);
+        System.out.println("The n'th term of the Fibonacci Sequence is " + recursion(10) + "!");
+    }
+
+}
+
